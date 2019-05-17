982fb60b99d97216ecf519fc924f8965e9f1abab
Vladimir Gusar <cancer@greendoubt.org>
2019-05-17 18:13:57 +0300

    add todos-list code

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class TodoList extends Component {
+
+	state = {
+		data: []
+	};
+
+	componentDidMount() {
+		axios.get("http://localhost:3001/test")
+			.then(response => {
+				this.setState({data: response.data});
+			})
+			.catch(error => {
+				console.log(error);
+			})
+	};
+
 	render() {
 		return (
 			<>
-				<p>Welcome to Todos List Component!!</p>
+				<h3>Todos List</h3>
+				<table className="table table-striped" style={{ marginTop: 20 }}>
+					<thead>
+						<tr>
+							<th>Description</th>
+							<th>Responsible</th>
+							<th>Priority</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+					{ this.todoList() }
+					</tbody>
+				</table>
 			</>
 		)
 	}
