66123e18f2c3984bbf48147a5b8c4e853c3f7fdd
Vladimir Gusar <cancer@greendoubt.org>
2019-05-17 14:40:49 +0300

    refactoring all code

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodoList extends Component {
+	render() {
+		return (
+			<>
+				<p>Welcome to Todos List Component!!</p>
+			</>
+		)
+	}
+}
