3dd9d905da4d198f6d53570cee8ba95fb26c478f
Vladimir Gusar <cancer@greendoubt.org>
2019-05-19 15:39:59 +0300

    add edit todo task

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import '../App.css';
+
 const Todo = props => (
 	<tr>
-		<td>{ props.todo.todo_description }</td>
-		<td>{ props.todo.todo_responsible }</td>
-		<td>{ props.todo.todo_priority }</td>
+		<td className={props.todo.todo_completed ? 'completed' : ''}>{ props.todo.todo_description }</td>
+		<td className={props.todo.todo_completed ? 'completed' : ''}>{ props.todo.todo_responsible }</td>
+		<td className={props.todo.todo_completed ? 'completed' : ''}>{ props.todo.todo_priority }</td>
 		<td>
-			<Link to={`/edit/ ${props.todo._id}`}>Edit</Link>
+			<Link to={`/edit/${props.todo._id}`}>Edit</Link>
 		</td>
 	</tr>
 );
