3ba48e35720382013cccf0dcf143f92793ee5edc
Vladimir Gusar <cancer@greendoubt.org>
2019-05-21 19:13:38 +0300

    add thunk in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,5 +4,6 @@ import { reducer } from './reducer';
 
 export const store = createStore(
 	reducer, /*preloadedState, */
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+	applyMiddleware(thunk)
 );
