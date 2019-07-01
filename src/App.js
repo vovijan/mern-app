13117c30b6f6541e0dc0dd1a81eb3711e8e57a03
Vladimir Gusar <cancer@greendoubt.org>
2019-07-01 10:09:59 +0300

    delete App css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-
-import './App.css';
-
+import styled from 'styled-components';
 import GroupListContainer from './containers/GroupList.container';
 import CreateGroupContainer from './containers/CreateGroup.container';
 import { Col, Container, Row } from "react-bootstrap";
 import SidebarContainer from "./containers/Sidebar.container";
+
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  background-color: #f5f5f5;
+`;
 
 export default class App extends Component {
   render() {
@@ -21,13 +28,13 @@ export default class App extends Component {
           </Col>
         </Row>
 
-        <footer className="footer">
+        <Footer>
           <Container>
             <span className="text-muted">
               MERN App (Todo) with Bootstrap 4
             </span>
           </Container>
-        </footer>
+        </Footer>
       </Container>
     )
   }
